051df4cf5d627cf2914b7aa24aaef107cc0c1661
Jeff Farr <jfarr@sentientdecisionscience.com>
2016-01-19 14:55:21 -0500

    Initial Commit

---
+++ GitWorkflow-JFarr/GitWorkflow-JFarr/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitWorkflow_JFarr
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // This is a comment made by JFarr
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
