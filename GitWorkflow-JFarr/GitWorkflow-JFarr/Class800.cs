f7581446a82785a68daecf58ad5886b083e4e4de
parvez-alam <parvez.alam@digitalavenues.com>
2016-02-04 11:16:29 +0530

    Prime-800 needs to upload on QA server

---
+++ GitWorkflow-JFarr/GitWorkflow-JFarr/Class800.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWorkflow_JFarr
+{
+    class Class800
+    {
+        public Class800() { } // this ticket has been completed need to deployed on QA.
+    }
+}
